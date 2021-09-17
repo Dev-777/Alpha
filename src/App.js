a14e3da2c69dbec8e2a1ccb1f28987f8e4e363dd
arsen-work <arsen172317@gmail.com>
2021-09-17 09:38:37 +0400

    some changes

--- src/App.js
+++ src/App.js
@@ -2,18 +2,20 @@ import React from "react";
 import { Route } from "react-router";
 import style from "./assets/scss/app.module.scss";
 import routes from "./routes";
+import TestOne from './test/TestOne'
 
 const App = () => {
   return (
     <div className={style.app}>
-      {routes.map((route, index) => (
+      <TestOne/>
+      {/*{routes.map((route, index) => (
         <Route
           key={index}
           path={route.path}
           exact={route.exact}
           children={<route.page />}
         />
-      ))}
+      ))}*/}
     </div>
   );
 };
--- src/test/TestOne.jsx
+++ src/test/TestOne.jsx
@@ -1,9 +1,30 @@
 import React from "react";
 
 const TestOne = () => {
+  console.log(Object.prototype, "obj");
+  obj;
   return (
     <>
-      <h1 style={{ color: "tomato", fontWeight: 900 }}>Exercise 30</h1>
+      <h1>JavaScript Object [ 18 Exercises with Solution ]</h1>
+      <h2 style={{ color: "tomato", fontWeight: 900 }}>Exercise 11</h2>
+      <p>JavaScript Object [ 18 Exercises with Solution ], cool exercise []</p>
+      -------------------------------------------------------------------------
+      <p>
+        JavaScript String [ 49 Exercises with Solution ], cool exercise [23,25]
+      </p>
+      <p>JavaScript Array [ 44 Exercises with Solution ], cool exercise [8] </p>
+      <p>
+        JavaScript Functions [ 29 Exercises with Solution ],cool exercise
+        [3,8,24,27]
+      </p>
+      <p>
+        JavaScript fundamental [116 exercises with solution], cool exercise`
+        [188,234]
+      </p>
+      <p>
+        JavaScript Fundamental (ES6 version) Part-I [ 150 Exercises with
+        Solution ] cool exercise [22,71,96]
+      </p>
     </>
   );
 };
